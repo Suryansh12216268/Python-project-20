c5932e82e8fc7c9381e6e169d19bec35ec4d1dc7
Suryansh Chandel <suryanshchandel07@gmail.com>
2022-11-30 22:59:06 +0530

    Python project 20

---
+++ Pyhton Project 20.py
@@ -0,0 +1,21 @@
+x = 10
+print(x>6 and x<12)
+print(x>20 and x>6)
+print (x>20 or x>6)
+print (10>6 or 10<6)
+print(not(x))
+print(not(x>20 or x<10))
+print(not(x>20 and x<10))
+
+str = 'i am learning python'
+print('am' in str)
+str = " i am learning python"
+str2 = "i"
+print(str2 in str)
+print(str in str2)
+
+li1 = [1,2,3,4]
+print(1 in li1)
+print(1 not in li1)
+print(5 not in li1)
+print(5 in li1)
